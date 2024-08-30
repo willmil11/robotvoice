e563c5f613d2949054b9e0e39b8223438eb8b7bd
willmil11 <willmil1110@gmail.com>
2024-08-30 17:04:50 +0200

    save

---
+++ generate.py
@@ -0,0 +1,13 @@
+import torch
+from TTS.api import TTS
+
+# Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# List available 🐸TTS models
+print(TTS().list_models())
+
+# Init TTS
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+
+tts.tts_to_file(text="Hello world!", speaker_wav="robot-voice-sample.wav", language="en", file_path="output.wav")
